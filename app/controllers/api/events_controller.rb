d07df8441503377446184e2270f1c0344827018b
YOUR NAME <muvanlier@gmail.com>
2017-07-10 20:52:14 +0200

    Add API index

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,7 +1,51 @@
 class Api::EventsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     render status: 200, json: {
       events: Event.all
     }.to_json
   end
+
+  def show
+    event = Event.find(event_params[:id])
+
+    render status: 200, json: {
+      event: event
+    }.to_json
+  end
+
+
+
+  def create
+   event = event.new(event_params)
+
+   if event.save
+     render status: 201, json: {
+       message: "event successfully created",
+       event: event
+     }.to_json
+   else
+     render status: 422, json: {
+       errors: event.errors
+     }.to_json
+   end
+ end
+
+ def destroy
+   event = Event.find(event_params[:id])
+   event.destroy
+
+   render status: 200, json: {
+     message: "Event has been deleted"
+   }.to_json
+ end
+
+   private
+ def event_params
+   event_params.require(:event).permit(:name,
+   :description, :location, :price, :capacity,
+   :includes_food, :includes_drinks, :starts_at, :ends_at,
+   :active, :user ,:themes)
+ end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_and_belongs_to_many :themes
 
-  has_many :photos
+  has_many :photos, dependent: :destroy
   has_many :registration, dependent: :destroy
 
   has_many :guests, through: :registration, source: :user
